8062a0aaed8a1966d002b0d84cd82378e8dbd49e
Guilherme Aldeia <guilherme.seidyo@gmail.com>
2024-02-07 09:34:18 -0300

    Updated example to show how to use the get_hashes

--- example.py
+++ example.py
@@ -11,6 +11,14 @@ lsh = LSHash(hash_size=k, input_dim=d, num_hashtables=L)
 # index vector
 lsh.index([2,3,4,5,6,7,8,9])
 
+# get the binary hash for an input point by iterating through all tables
+binary_hashes = lsh.get_hashes([2,3,4,5,6,7,8,9])
+print("hash representation", binary_hashes)
+
+# checking that each table stores the same input vector with different keys
+for key, table in zip(binary_hashes, lsh.hash_tables):
+    print(key, table.get_list(key))
+
 # index vector and extra data
 lsh.index([10,12,99,1,5,31,2,3], extra_data="vec1")
 lsh.index([10,11,94,1,4,31,2,3], extra_data="vec2")
@@ -31,7 +39,6 @@ top_n = 3
 nn = lsh.query([10,12,99,1,5,30,1,1], num_results=top_n, distance_func="euclidean")
 for ((vec,extra_data),distance) in nn:
     print(vec, extra_data, distance)
-
 
 # InMemoryStorage
 lsh = LSHash(hash_size=k, input_dim=d, num_hashtables=L,
